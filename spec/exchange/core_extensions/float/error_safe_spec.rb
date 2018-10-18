c0eaa5129a5cb9ceee2cb5953b1c46fc2d0f0e8b
Regan McEntyre <regan@pocketsmith.com>
2018-10-18 18:05:39 +1300

    Remove tests conditional on Ruby MRI and version
    Going forward we're not going to support Ruby < 2.1, so we can safely remove these conditional expects and the check on Ruby version. The version detection was hardcoding Ruby 2.1 and 2.2, so the test suite fails on any newer rubies.

--- spec/exchange/core_extensions/float/error_safe_spec.rb
+++ spec/exchange/core_extensions/float/error_safe_spec.rb
@@ -2,7 +2,6 @@
 require 'spec_helper'
 
 describe "Exchange::ErrorSafe" do
-  let(:is_mri_21_or_greater) { ((RUBY_VERSION =~ /\A2.1/ || RUBY_VERSION =~ /\A2.2/) && RUBY_ENGINE == 'ruby') }
   let(:time) { Time.gm(2012,8,27) }
   before(:all) do
     Exchange.configuration = Exchange::Configuration.new { |c| c.cache = { :subclass => :no_cache } }
@@ -36,11 +35,7 @@ describe "Exchange::ErrorSafe" do
         expect((1.0e+25 + BigDecimal.new("9999999999999999900000000").in(:usd)).round.to_f).to eq(2.0e+25)
       end
       it "should not touch other operations" do
-        if is_mri_21_or_greater
-          expect((1.0e+25 + BigDecimal.new("9999999999999999900000000")).round).to eq(BigDecimal.new("0.199999999999999999E26"))
-        else
-          expect((1.0e+25 + BigDecimal.new("9999999999999999900000000")).round).to eq(20000000000000001811939328)
-        end
+        expect((1.0e+25 + BigDecimal.new("9999999999999999900000000")).round).to eq(BigDecimal.new("0.199999999999999999E26"))
       end
     end
     describe "-" do
@@ -48,11 +43,7 @@ describe "Exchange::ErrorSafe" do
         expect((1.0e+25 - BigDecimal.new("9999999999999999900000000").in(:usd)).round).to eq(100000000)
       end
       it "should not touch other operations" do
-        if is_mri_21_or_greater
-          expect(1.0e+25 - BigDecimal.new("9999999999999999900000000")).to eq(BigDecimal.new("0.1E9"))
-        else
-          expect(1.0e+25 - BigDecimal.new("9999999999999999900000000")).to eq(0)
-        end
+        expect(1.0e+25 - BigDecimal.new("9999999999999999900000000")).to eq(BigDecimal.new("0.1E9"))
       end
     end
   end
